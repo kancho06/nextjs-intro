c68320a2db1c214470b196d064ac6962d4ce8b4b
kancho06 <wotjd5792@gmail.com>
2022-04-30 01:58:21 +0900

    practice nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import {useState} from "react";
-import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
+import {useEffect, useState} from "react";
+
+const API_KEY = "ab3e5df4aa30034135f20b8581f2ba4e";
 
 export default function Home() {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const { results } = await (
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                )
+            ).json();
+            setMovies(results);
+        })();
+    }, []);
     return (
         <div>
-            <h1>Hello</h1>
+            <Seo title="Home"/>
+            {!movies && <h4>Loading...</h4>}
+            {movies.map(movie => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
     );
 }
